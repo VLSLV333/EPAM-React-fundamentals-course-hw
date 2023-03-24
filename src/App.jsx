d5afdd2e13504b41fb858a2c0ff1d0052210c763
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-03-24 13:38:54 +0200

    Ready for check HW_2

--- src/App.jsx
+++ src/App.jsx
@@ -6,51 +6,50 @@ import CoursesPage from './pages/CoursesPage';
 import CoursesInfoPage from './pages/CoursesInfoPage';
 import CreateCoursePage from './pages/CreateCoursePage';
 import ErrorPage from './pages/ErrorPage';
+import LoginPage, { action as loginAction } from './pages/LoginPage';
+import RegistrationPage, {
+	action as registrationAction,
+} from './pages/RegistrationPage';
 
-// import LoginPage, { action as loginAction } from './pages/LoginPage';
-
-// import RegistrationPage, {
-// 	action as registrationAction,
-// } from './pages/RegistrationPage';
-
-// import { tokenLoader, checkIfAuthentificated } from './util/authentication';
-// import { logOut } from './util/logout';
+import { tokenLoader, checkIfAuthentificated } from './util/authentication';
+import { logOut } from './util/logout';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <RootPage />,
-		// loader: tokenLoader,
+		loader: tokenLoader,
 		id: 'root',
 		errorElement: <ErrorPage />,
 		children: [
 			{
 				index: true,
 				element: <MainPage />,
+				loader: checkIfAuthentificated,
 			},
 			{
 				path: 'courses',
-				// loader: checkIfAuthentificated,
+				loader: checkIfAuthentificated,
 				children: [
 					{ index: true, element: <CoursesPage /> },
 					{ path: ':courseId', element: <CoursesInfoPage /> },
 					{ path: 'add', element: <CreateCoursePage /> },
 				],
 			},
-			// {
-			// 	path: 'registration',
-			// 	element: <RegistrationPage />,
-			// 	action: registrationAction,
-			// },
-			// {
-			// 	path: 'login',
-			// 	element: <LoginPage />,
-			// 	action: loginAction,
-			// },
-			// {
-			// 	path: 'logout',
-			// 	action: logOut,
-			// },
+			{
+				path: 'registration',
+				element: <RegistrationPage />,
+				action: registrationAction,
+			},
+			{
+				path: 'login',
+				element: <LoginPage />,
+				action: loginAction,
+			},
+			{
+				path: 'logout',
+				action: logOut,
+			},
 		],
 	},
 ]);
